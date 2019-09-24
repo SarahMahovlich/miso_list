09fc3d4f79727c8fe0df91f473ed54ac70f408c6
ChrisnNg <cristopherng@hotmail.com>
2019-09-24 21:30:17 +0000

    add links into schema and links into seeds

--- db/schema/02_books.sql
+++ db/schema/02_books.sql
@@ -5,5 +5,6 @@ CREATE TABLE books (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   name VARCHAR(255) NOT NULL,
-  context TEXT
+  context TEXT,
+  link VARCHAR(255)
 );
